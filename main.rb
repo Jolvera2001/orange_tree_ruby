98fc38988196b1d9de3f76ef044777a97925e569
Jolvera2001 <jolvera6@stedwards.edu>
2025-06-21 18:16:07 -0500

    trying to solve weird templating issue

--- main.rb
+++ main.rb
@@ -1,6 +1,8 @@
 require 'dotenv/load'
 require 'sinatra'
 require 'mongoid'
+require 'sanitize'
+require 'redcarpet'
 require 'haml'
 
 Mongoid.load!('config/mongoid.yml', :development)
@@ -15,4 +17,10 @@ get "/" do
   @blogs = Blog.order_by(:title => 1).limit(10).only(:title, :description)
 
   haml :index
+end
+
+get "/blog/:id" do
+  @blog = Blog.where(id: params[:id]).first
+
+  haml :blog
 end
